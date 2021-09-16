93cc280bbd6fc3abdc082a041d3c708e5821a35a
Ian Johannesen <ij@opsplaza.com>
2021-09-16 18:25:21 +0200

    bugs(pkgs): invalid double version attr

--- pkgs/vim-plugins/nvim-treesitter-angular/default.nix
+++ pkgs/vim-plugins/nvim-treesitter-angular/default.nix
@@ -3,7 +3,6 @@ pkgs.vimUtils.buildVimPluginFrom2Nix {
   name = "nvim-treesitter-angular-master";
   version = "master";
   pname = "nvim-treesitter-angular";
-  version = "master";
   src = pkgs.fetchFromGitHub {
     inherit (sources.nvim-treesitter-angular) owner repo rev sha256;
   };
