10ca256f2b2445ebc443b2ac582b01969a6ca9df
Ian Johannesen <ij@opsplaza.com>
2021-09-16 18:52:47 +0200

    bugs(pkgs): simplication of composer2nix

--- pkgs/php-packages/composer2nix/default.nix
+++ pkgs/php-packages/composer2nix/default.nix
@@ -1,7 +1,7 @@
-{ sources, pkgs, systems ? [ "x86_64-linux" ], ... }:
-pkgs.lib.genAttrs systems (system: (import "${sources.composer2nix}/default.nix" {
-  inherit pkgs system;
+{ sources, pkgs, ... }:
+(import "${sources.composer2nix}/default.nix" {
+  inherit pkgs;
   noDev = true;
 }).override {
   executable = true;
-})
+}
