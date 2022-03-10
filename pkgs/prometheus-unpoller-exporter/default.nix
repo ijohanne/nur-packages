0a512da1e9d6c0a9b2ec68df1a9d6e7cb8afdd49
Ian Johannesen <ij@opsplaza.com>
2022-03-10 16:05:49 +0100

    chores(pkgs): update unpoller

--- pkgs/prometheus-unpoller-exporter/default.nix
+++ pkgs/prometheus-unpoller-exporter/default.nix
@@ -3,7 +3,5 @@ pkgs.buildGoModule rec {
   name = "prometheus-unpoller-exporter";
   src = sources.unpoller;
   vendorSha256 = "sha256-uqpF0By3GIMGwTZuQoovLaitvybImCqUsli/G01v/GA=";
-  checkFlags = [ "-short" ];
-  deleteVendor = true;
   proxyVendor = true;
 }
