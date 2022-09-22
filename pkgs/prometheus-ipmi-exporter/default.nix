9e9ed6a0999c5d40fb5a7789bd43553ba1da24e6
Ian Johannesen <ij@opsplaza.com>
2022-09-22 15:45:14 +0200

    features(pkg): update prometheus-ipmi-exporter

--- pkgs/prometheus-ipmi-exporter/default.nix
+++ pkgs/prometheus-ipmi-exporter/default.nix
@@ -1,7 +1,7 @@
 { sources, pkgs, ... }:
-pkgs.buildGo116Module rec {
+pkgs.buildGo118Module rec {
   name = "prometheus-ipmi-exporter";
   src = sources.ipmi_exporter;
-  vendorSha256 = "0axsgazbqlss2kyr4xzsb4h4794z2p2vhp1fgfd8xvf63i42n27a";
+  vendorSha256 = "sha256-ywwrsiL3XIWP44GlRRF3ProX4Gat7jJY1OIoD+v/4sM=";
   proxyVendor = true;
 }
